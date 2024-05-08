bc268508ab52fc7b2a494b3701ca86c479831c6f
HuyTran0996 <tranviquanghuy@gmail.com>
2024-05-08 16:37:51 +0700

    set up axios

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 This is a random quote machine for FreeCodeCamp's Front End Development Libraries certification.
 https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-random-quote-machine
+
+Demo app from freeCodeCamp: https://random-quote-machine.freecodecamp.rocks/
---
+++ src/app/apiService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const apiService = axios.create({
+  baseURL:
+    "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+});
+
+apiService.interceptors.request.use((request) => {
+  console.log("Start request", request);
+  return request;
+});
+
+apiService.interceptors.response.use(
+  (response) => {
+    console.log("Response", response);
+    return response;
+  },
+  (error) => {
+    console.log("RESPONSE ERROR", error);
+    return Promise.reject(error.response.data);
+  }
+);
+
+export { apiService };
